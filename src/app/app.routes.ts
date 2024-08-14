eaae59e865d7afd22e767a156f989e4796856d21
crice88 <colinrice07@gmail.com>
2024-08-14 12:13:32 -0700

    initial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+
+export const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'home', component: HomeComponent }
+];
